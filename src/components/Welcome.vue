760c4a2a655c93d82f19cee0e9106d2230c96ba9
Elly McIntyre <eloise.mcintyre@figured.com>
2019-12-20 17:10:55 +1300

    fix lil bug

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -106,10 +106,45 @@
 import Footer from './Footer.vue'
 
 export default {
+    data() {
+      return {
+        name: '',
+        nameState: null,
+        submittedNames: []
+      }
+    },
     components: {
         Footer
+    },
+    methods: {
+      checkFormValidity() {
+        const valid = this.$refs.form.checkValidity()
+        this.nameState = valid ? 'valid' : 'invalid'
+        return valid
+      },
+      resetModal() {
+        this.name = ''
+        this.nameState = null
+      },
+      handleOk(bvModalEvt) {
+        // Prevent modal from closing
+        bvModalEvt.preventDefault()
+        // Trigger submit handler
+        this.handleSubmit()
+      },
+      handleSubmit() {
+        // Exit when the form isn't valid
+        if (!this.checkFormValidity()) {
+          return
+        }
+        // Push the name to submitted names
+        this.submittedNames.push(this.name)
+        // Hide the modal manually
+        this.$nextTick(() => {
+          this.$refs.modal.hide()
+        })
+      }
     }
-   
 }
 </script>
 
